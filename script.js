216f8edf004394ca100800a23f411f929133a8f9
kevin Chen <kc2029@hw.ac.uk>
2023-02-19 19:08:00 +0000

    V1

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 // The svg
 const svg = d3.select("#covidmap"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+  width = +svg.attr("width"),
+  height = +svg.attr("height");
 
 // Map and projection
-const projection = d3.geoNaturalEarth1()
-    .scale(width / 1.3 / Math.PI)
-    .translate([width / 2, height / 2])
+const projection = d3.geoEquirectangular();
 
-// Load external data and boot
 
+//scale the worldmap
+function mapScale(data) {
+  projection.fitExtent([[50, 00], [1200, 500]], data);
+}
+
+
+//load the geo json and draw the map
 d3.json("https://raw.githubusercontent.com/kc2029/F21DV_map/main/custom.geo.json").then(function (data) {
 
-    // Draw the map
-    svg.append("g")
-        .selectAll("path")
-        .data(data.features)
-        .join("path")
-        .attr("fill", "#69b3a2")
-        .attr("d", d3.geoPath()
-            .projection(projection)
-        )
-        .style("stroke", "#fff")
+  mapScale(data);
 
+  // Draw the map
+  svg.append("g")
+
+    .selectAll("path")
+    .data(data.features)
+    .join("path")
+    .attr("fill", "grey") //map color, background?
+
+    .attr("d", d3.geoPath()
+      .projection(projection)
+    )
+    .style("stroke", "white") //country border color
 })
